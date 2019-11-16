c73e8aac2db972193a5874d1bcb4ab197b7e1511
lcube <info@lcube.fr>
2019-11-16 15:31:57 +0100

    add drawer to AppNavigation

--- src/components/AppNavigation.vue
+++ src/components/AppNavigation.vue
@@ -1,13 +1,29 @@
 <template>
+    <span>
+        <v-navigation-drawer app v-model="drawer" class="light-blue darken-4" dark disable-resize-watcher>
+            <v-list>
+                <template v-for="(item, index) in items">
+                    <v-list-tile :key="index">
+                        <v-list-tile-content>
+                            {{item.title}}
+                        </v-list-tile-content>
+                    </v-list-tile>
+                    <v-divider :key="`divider-${index}`"></v-divider>
+                </template>
+            </v-list>
+        </v-navigation-drawer>
+
         <v-app-bar app color="light-blue darken-1" dark>
-            <v-app-bar-nav-icon></v-app-bar-nav-icon>
+            <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+            <v-spacer class="hidden-md-and-up"></v-spacer>
             <v-toolbar-title>{{appTitle}}</v-toolbar-title>
-            <v-spacer></v-spacer>
-            <v-btn icon>
+            <v-spacer class="hidden-sm-and-down"></v-spacer>
+            <v-btn icon class="hidden-sm-and-down">
                 <v-icon>mdi-magnify</v-icon>
             </v-btn>
-            <v-btn text>SIGN IN</v-btn>
+            <v-btn text class="hidden-sm-and-down">LOGIN</v-btn>
         </v-app-bar>
+    </span>
 </template>
 
 <script lang="ts">
@@ -18,7 +34,20 @@ export default Vue.extend({
     data() {
         return {
             appTitle: 'My Songbook',
+            drawer: false,
+            items: [
+                { title: 'Songs' },
+                { title: 'Artists' },
+                { title: 'Login' }
+            ]
         };
     },
 });
 </script>
+
+<style scoped>
+a {
+    color: white;
+    text-decoration: none;
+}
+</style>
