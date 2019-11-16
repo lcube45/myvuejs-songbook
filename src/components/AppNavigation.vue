7b944cd5953f7146443ef84f73a126b5b79a51eb
lcube <info@lcube.fr>
2019-11-16 15:09:20 +0100

    add appnavigation component

---
+++ src/components/AppNavigation.vue
@@ -0,0 +1,24 @@
+<template>
+        <v-app-bar app color="light-blue darken-1" dark>
+            <v-app-bar-nav-icon></v-app-bar-nav-icon>
+            <v-toolbar-title>{{appTitle}}</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn icon>
+                <v-icon>mdi-magnify</v-icon>
+            </v-btn>
+            <v-btn text>SIGN IN</v-btn>
+        </v-app-bar>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+    name: 'AppNavigation',
+    data() {
+        return {
+            appTitle: 'My Songbook',
+        };
+    },
+});
+</script>
